197041914871bf744714a4ef8d51bc9f22d33be9
Marie-Christine Desplat <kiki_online@live.se>
2019-02-13 23:56:02 +0100

    Begin exercise 1.2

--- week2/JavaScript2-week2.js
+++ week2/JavaScript2-week2.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // 1.1
 /*
 function doubleOddNumbers(numbers) {
@@ -27,4 +29,87 @@ function doubleOddNumbers(someArray) {
     });
 }
 const someNumbers = [1, 2, 3, 4, 1, 8, 13];
-console.log(doubleOddNumbers(someNumbers)); // ==> [2, 6, 10, 18]
+console.log(doubleOddNumbers(someNumbers)); // ==> [2, 6, 2, 26]
+
+// ******************************************
+// 1.2
+
+const monday = [
+  {
+    name: 'Write a summary HTML/CSS',
+    category: 'summary',
+
+    duration: 180,
+  },
+  {
+    name: 'Some web development',
+    category: 'development',
+    duration: 120,
+  },
+  {
+    name: 'Fix homework for class10',
+    category: 'homework',
+    duration: 20,
+  },
+  {
+    name: 'Talk to a lot of people',
+    category: 'talk',
+    duration: 1.0,
+  },
+];
+
+const tuesday = [
+  {
+    name: 'Keep writing summary',
+    category: 'summary',
+    duration: 1.0,
+  },
+  {
+    name: 'Some more web development',
+    category: 'development',
+    duration: 180,
+  },
+  {
+    name: 'Staring out the window',
+    category: 'miscellaneous',
+    duration: 10,
+  },
+  {
+    name: 'Talk to a lot of people',
+    category: 'talk',
+    duration: 1.0,
+  },
+  {
+    name: 'Look at application assignments new students',
+    category: 'application',
+    duration: 40,
+  },
+];
+
+// group monday and tuesday tasks
+const tasks = monday.concat(tuesday);
+//console.log(tasks);
+
+// group identical keys
+let addTasks = tasks.reduce(function(acc, val) {
+  var o = acc
+    .filter(function(obj) {
+      return obj.name == val.category;
+    })
+    .pop() || { name: val.category, duration: 0 };
+
+  o.duration += val.duration;
+  acc.push(o);
+  return acc;
+}, []);
+
+let totalDuration = addTasks.filter(function(itm, i, a) {
+  return i == a.indexOf(itm);
+});
+// console.log(totalDuration);
+
+// write durations in hours
+let duration = totalDuration.map(x => x.duration / 60);
+console.log(duration);
+
+// filter out durations shorter than 2 hours
