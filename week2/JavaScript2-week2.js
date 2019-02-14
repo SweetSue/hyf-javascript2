7924cf00aa96ef2c0c9f749d15a354b986142ecd
Marie-Christine Desplat <kiki_online@live.se>
2019-02-14 12:34:00 +0100

    Insert exercise 1.1 and finish exercise 1.2
    exercise 1.2: delete unnecessary category key, simplify functions.

--- week2/JavaScript2-week2.js
+++ week2/JavaScript2-week2.js
@@ -1,23 +1,48 @@
+// EXERCISE 1.1
+function doubleOddNumbers(numbers) {
+  const newNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 !== 0) {
+      newNumbers.push(numbers[i] * 2);
+    }
+  }
+  return newNumbers;
+}
+
+const myNumbers = [1, 2, 3, 4];
+console.log(doubleOddNumbers(myNumbers)); // ==> [2, 6]
+// ******************************************
+// NEW WAY TO DO IT
+function dOddNumbers(someArray) {
+  return someArray
+    .filter(function(val) {
+      return val % 2 !== 0;
+    })
+    .map(function(val) {
+      return val * 2;
+    });
+}
+// doubleOddNumbers(arr)
+const someNumbers = [1, 2, 3, 4, 1, 8, 13];
+console.log(dOddNumbers(someNumbers)); // ==> [2, 6, 2,26]
+
+// ******************************************
+// EXERCISE 1.2
 const monday = [
   {
     name: 'Write a summary HTML/CSS',
-    category: 'Writing summary',
-
     duration: 180,
   },
   {
     name: 'Some web development',
-    category: 'Web development',
     duration: 120,
   },
   {
     name: 'Fix homework for class10',
-    category: 'homework',
     duration: 20,
   },
   {
     name: 'Talk to a lot of people',
-    category: 'talk',
     duration: 1.0,
   },
 ];
@@ -25,59 +50,61 @@ const monday = [
 const tuesday = [
   {
     name: 'Keep writing summary',
-    category: 'Writing summary',
     duration: 1.0,
   },
   {
     name: 'Some more web development',
-    category: 'Web development',
     duration: 180,
   },
   {
     name: 'Staring out the window',
-    category: 'miscellaneous',
     duration: 10,
   },
   {
     name: 'Talk to a lot of people',
-    category: 'talk',
     duration: 1.0,
   },
   {
     name: 'Look at application assignments new students',
-    category: 'application',
     duration: 40,
   },
 ];
+var tasks = [monday, tuesday];
+var result = tasks
+  .reduce(function(taskGroup, current) {
+    return taskGroup.concat(current);
 
-// group monday and tuesday tasks
-const tasks = monday.concat(tuesday);
-//console.log(tasks);
+    // convert task duration from minutes to hours
+  })
+  .map(function(task) {
+    return task.duration / 60;
 
-// group identical keys
-let addTasks = tasks.reduce(function(acc, val) {
-  var o = acc
-    .filter(function(obj) {
-      return obj.name == val.category;
-    })
-    .pop() || { name: val.category, duration: 0 };
+    // filter the tasks longer than 2 hours
+  })
+  .filter(function(duration) {
+    return duration >= 2;
+    //  calculate the fee for each long task
+  })
+  .map(function(duration) {
+    return duration * 20;
+    // calculate the total fee
+  })
+  .reduce(function(taskGroup, current) {
+    return [+taskGroup + +current];
+    // insert the dollar currency sign before the total fee
+  })
+  .map(function(dollar_amount) {
+    return '$' + dollar_amount.toFixed(2);
+  })
+  // format the total fee without brackets
+  .reduce(function(formatted_dollar_amount) {
+    return formatted_dollar_amount;
+  });
 
-  o.duration += val.duration;
-  acc.push(o);
-  return acc;
-}, []);
+console.log(result);
 
-let totalDuration = addTasks.filter(function(itm, i, a) {
-  return i == a.indexOf(itm);
-});
-// console.log(totalDuration);
+// EXPLAINATION HERE
+// https://code.tutsplus.com/tutorials/how-to-use-map-filter-reduce-in-javascript--cms-26209
 
-// filter out durations shorter than 2 hours
-let longTasks = totalDuration.filter(function(e) {
-  return e.duration > 120;
-});
-// console.log(longTasks);
-
-// write durations in hours
-let duration = longTasks.map(x => x.duration / 60);
-console.log(duration);
+// MORE HERE
+// https://codeburst.io/learn-understand-javascripts-filter-function-bde87bce206
